f29ca53fccaa85e914ab97ab629137e6bcc30fcc
Thomas Fettig <thomasmfettig@gmail.com>
2022-05-15 17:34:02 -0600

    Add completed book and shelf tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,67 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`
 }
+
+
+function buildMainCharacter(characterName, characterAge, characterPronouns) {
+  return {
+    name: characterName,
+    age: characterAge,
+    pronouns: characterPronouns,
+  }
+}
+
+
+function saveReview(addReview, reviews) {
+    for (var i = 0; i < reviews.length; i++) {
+      if (addReview === reviews[i]) {
+        return reviews
+    }
+  }     return reviews.push(addReview)
+}
+// Another way to pass this test using the .includes method:
+//   if (reviews.includes(addReview)) {
+//     return reviews
+//   }  else {
+//     reviews.push(addReview)
+//   }
+// }
+
+
+function calculatePageCount(bookTitle) {
+  return 20 * bookTitle.length
+}
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: 20 * bookTitle.length,
+    genre: bookGenre,
+  }
+}
+
+
+function writeBook(dragonTitle, dragonCharacter, dragonGenre) {
+  return {
+    title: dragonTitle,
+    mainCharacter: dragonCharacter,
+    pageCount: 20 * dragonTitle.length,
+    genre: dragonGenre,
+  }
+}
+
+
+function editBook(reducedBook) {
+  return reducedBook.pageCount = reducedBook.pageCount * .75
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,43 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+}
+
+
+function unshelfBook(bookName, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookName) {
+      shelf.splice(i, 1)
+    }
+  }
+}
+
+
+function listTitles(shelfTitles) {
+  var bookTitles = []
+    for (var i = 0; i < shelfTitles.length; i++) {
+      bookTitles.push(shelfTitles[i].title)
+  }
+      return bookTitles.join(", ")
+}
+
+
+function searchShelf(shelf, bookName) {
+    var onShelf = null
+      for (var i = 0; i < shelf.length; i++) {
+        if (shelf[i].title.includes(bookName)) {
+          return onShelf = true
+      } else {
+           onShelf = false
+      }
+  }       return onShelf
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
